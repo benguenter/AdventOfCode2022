9648e294be50de58fbc178ab002005d0d9630d8c
Benjamin Guenter <benjamin.guenter@autostoresystem.com>
2022-12-04 22:32:34 -0700

    Add Day 1 solution

---
+++ src/day1.rs
@@ -0,0 +1,34 @@
+use std::{
+    fs::File,
+    io::{self, Read},
+    path::Path,
+};
+
+pub fn main() {
+    if let Ok(contents) = read_file("./input.txt") {
+        let mut calorie_groups: Vec<i32> = contents
+            .split("\r\n\r\n")
+            .map(|line| {
+                line.split("\r\n")
+                    .filter_map(|chunk| str::parse::<i32>(chunk).ok())
+                    .sum()
+            })
+            .collect();
+        calorie_groups.sort();
+        let max_calorie_groups: i32 = calorie_groups[calorie_groups.len() - 3..].iter().sum();
+        println!("{:?}", max_calorie_groups);
+    } else {
+        println!("Could not find file ./input.txt");
+    }
+}
+
+fn read_file<P>(filename: P) -> io::Result<String>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let mut reader = io::BufReader::new(file);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
